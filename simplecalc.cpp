45544dd889b2a96cbd25dfe1b2bb1d160b8fd3f4
Adrian Calvillo <94008133+AdriantheCode@users.noreply.github.com>
2024-08-31 22:55:45 +0000

    Title and instructions

--- simplecalc.cpp
+++ simplecalc.cpp
@@ -4,8 +4,9 @@ using namespace std;
 
 int main()
 {
-
     std::cout <<"hello world\n";
-
+    std::cout <<"This is a Simple Console-Base Calculator\n";
+    std::cout <<"Enter an operation to perform. Use the following format example guide: a+b | a-b | a*b | a/b\n"
+    
 }
  
