55561c5a07074d04cff974435b4c50240f66e820
Adrian Calvillo <94008133+AdriantheCode@users.noreply.github.com>
2024-09-01 00:58:01 +0000

    Added calculator heading, defined calculator class

--- simplecalc.cpp
+++ simplecalc.cpp
@@ -4,9 +4,10 @@ using namespace std;
 
 int main()
 {
-    std::cout <<"hello world\n";
-    std::cout <<"This is a Simple Console-Base Calculator\n";
-    std::cout <<"Enter an operation to perform. Use the following format example guide: a+b | a-b | a*b | a/b\n"
+    cout <<"hello world\n";
+    cout <<"This is a Simple Console-Base Calculator\n";
+    cout <<"Enter an operation to perform. Use the following format example guide: a+b | a-b | a*b | a/b\n";
     
+    return 0;
 }
  
